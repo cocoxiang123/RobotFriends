b854cefedc2cf9f604558220d6e98fa4c1fcc977
cocoxiang123 <cocoxiang123@users.noreply.github.com>
2020-09-01 15:45:03 +1000

    init

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id:1,
+        name:'Leanne Graham',
+        username:'Bret',
+        email:'Leanne@gmail.com'
+    },
+    {
+        id:2,
+        name:'Ervin Howell',
+        username:'Antontee',
+        email:'antonee@gmail.com'
+    },
+    {
+        id:3,
+        name:'clemertine Bauch',
+        username:'Samatha',
+        email:'smatha@gmail.com'
+    },
+    {
+        id:4,
+        name:'May Graham',
+        username:'Bret',
+        email:'May@gmail.com'
+    },
+    {
+        id:5,
+        name:'Bedy Howell',
+        username:'Antontee',
+        email:'Bady@gmail.com'
+    },
+    {
+        id:6,
+        name:'Popli Bauch',
+        username:'pskd',
+        email:'Poplo@gmail.com'
+    },
+    {
+        id:7,
+        name:'May Grey',
+        username:'dks',
+        email:'Grey@gmail.com'
+    },
+    {
+        id:8,
+        name:'Bedy Opsd',
+        username:'asdf',
+        email:'pppp@gmail.com'
+    },
+    {
+        id:9,
+        name:'Popli Bodlsd',
+        username:'pskd',
+        email:'Bodsf@gmail.com'
+    },
+    {
+        id:10,
+        name:'Kosdfing fish',
+        username:'pskd',
+        email:'fish@gmail.com'
+    }
+]
